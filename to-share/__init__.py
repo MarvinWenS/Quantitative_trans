bcf87872f4832c0023f764d1a49c79b8fea7f370
Marvin,Wen <46546924@qq.com>
2022-06-30 00:14:09 +0800

    demo test

---
+++ to-share/__init__.py
@@ -0,0 +1,5 @@
+import tushare as ts
+
+token = '8bb1f3d757ff5f000e6af077d19d6eabe5de900b8dc87a85643a1d38'
+
+pro = ts.pro_api(token)
---
+++ to-share/test.py
@@ -0,0 +1,4 @@
+import toshare as ts
+
+token = '8bb1f3d757ff5f000e6af077d19d6eabe5de900b8dc87a85643a1d38'
+toshare.get_hist_data('600848')
